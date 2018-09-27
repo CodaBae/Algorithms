3f0fc7b3fa0d15f3971a9461412cb6f78dac1367
Your Name <shalom.mat40@gmail.com>
2018-09-27 17:43:23 +0300

    day 14 and 15

---
+++ getAllKeys/Index.js
@@ -0,0 +1,39 @@
+
+// Write a function called "getAllKeys" which returns an array of all the input object's keys.
+// Example input:
+// {
+//   name : 'Sam',
+//   age : 25,
+//   hasPets : true
+// }
+// Function's return value (output) :
+// ['name', 'age', 'hasPets']
+// Do not use "Object.keys" to solve this prompt.
+// Note that your function should be able to handle any object passed in it.
+// E.g. it should also handle an input like:
+// {
+//   a : 'a',
+//   number : 11,
+//   hungry : true,
+//   grammyWins : 1
+// }
+// Function's return value (output):
+// ['a', 'number', 'hungry', 'grammyWins']
+// Starter Code:
+// function getAllKeys(obj) {
+//   // your code here
+// }
+
+
+var object = { a : 'a', number : 11, hungry : true, grammyWins : 1 }
+
+function getAllKeys(object) {
+  newArray = []
+  for (var key in object) {
+   newArray.push(key)
+  }
+  return newArray
+}
+output = getAllKeys(object)
+console.log(output)
+
